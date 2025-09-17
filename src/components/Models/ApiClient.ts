5b3fdf1d78026ed91822ec6331ef01e8632cd148
Nikolay Boykov <nick.boickov@yandex.ru>
2025-09-17 22:35:17 +0300

    PR8_GET_fixed

--- src/components/Models/ApiClient.ts
+++ src/components/Models/ApiClient.ts
@@ -1,27 +1,27 @@
 import { IApi, IProduct } from "../../types";
 
 export class ApiClient {
-    private api: IApi;
-    constructor (api: IApi) {
-        this.api = api;
-    }
-    async getProducts (): Promise<IProduct[]> {
-        try {
-      const products = this.api.get<IProduct[]>('/product/');
-     return  products;
+  private api: IApi;
+  constructor(api: IApi) {
+    this.api = api;
+  }
+  async getProducts(): Promise<IProduct[]> {
+    try {
+      const products = this.api.get<IProduct[]>("/product/");
+      return products;
     } catch (error) {
-        console.error('Ошибка при получении товаров:', error);
-        return [];
-}
-}
-
-    async createOrder(orderData: object): Promise<object> {
-        try {
-            const response = this.api.post('/order/', orderData);
-            return response;
-        } catch (error) {
-            console.error('Ошибка при создании заказа:', error);
-            return [];
-        }
+      console.error("Ошибка при получении товаров:", error);
+      return [];
     }
+  }
+
+  async createOrder(orderData: object): Promise<object> {
+    try {
+      const response: object = await this.api.post("/order/", orderData);
+      return response;
+    } catch (error) {
+      console.error("Ошибка при создании заказа:", error);
+      return { error: "...", details: error };
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,43 @@
-import './scss/styles.scss';
-import { Api } from './components/base/Api';
-import { API_URL } from './utils/constants';
-import { ApiClient } from './components/Models/ApiClient';
-import { ProductCatalog } from './components/Models/ProductCatalog';
-
-
-
+import "./scss/styles.scss";
+import { Api } from "./components/base/Api";
+import { API_URL } from "./utils/constants";
+import { ApiClient } from "./components/Models/ApiClient";
+import { ProductCatalog } from "./components/Models/ProductCatalog";
 
 const api = new Api(API_URL);
 const apiClient = new ApiClient(api);
 const productsModel = new ProductCatalog();
 
-apiClient.getProducts()
-    .then(products => {
-        console.log('Массив данных с товарами', products);
-        
-        productsModel.setProducts(products);
-        
-        const savedProducts = productsModel.getProducts();
-        console.log('Товары сохранены в каталоге:', savedProducts);
-    })
-    .catch(error => {
-        console.error('Ошибка загрузки товаров:', error);
-    });
+apiClient
+  .getProducts()
+  .then((products) => {
+    console.log("Массив данных с товарами", products);
 
-apiClient.createOrder()
-    .then(response => {
-        console.log('Заказ создан успешно:', response);
-    })
-    .catch(error => {
-        console.error('Ошибка при создании заказа:', error);
-    });
+    productsModel.setProducts(products);
+
+    const savedProducts = productsModel.getProducts();
+    console.log("Товары сохранены в каталоге:", savedProducts);
+  })
+  .catch((error) => {
+    console.error("Ошибка загрузки товаров:", error);
+  });
+
+const orderData = {
+    "payment": "online",
+    "email": "test@test.ru",
+    "phone": "+71234567890",
+    "address": "Spb Vosstania 1",
+    "total": 2200,
+    "items": [
+        "854cef69-976d-4c2a-a18c-2aa45046c390",
+        "c101ab44-ed99-4a54-990d-47aa2bb4e7d9"
+    ]
+};
+apiClient
+  .createOrder(orderData)
+  .then((response) => {
+    console.log("Заказ создан успешно:", response);
+  })
+  .catch((error) => {
+    console.error("Ошибка при создании заказа:", error);
+  });
