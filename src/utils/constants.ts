523b27514781ecc110a559b9942231c65d444b4d
Nick Boickov <nick.boickov@yandex.ru>
2025-10-09 18:26:20 +0300

    PR9_header

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,14 @@ export const API_URL = `https://larek-api.nomoreparties.co/api/weblarek`;
 которое хранится в объекте товара. */
 export const CDN_URL = `https://larek-api.nomoreparties.co/content/weblarek`;
 
+export const categoryMap = {
+  'софт-скил': 'card__category_soft',
+  'хард-скил': 'card__category_hard',
+  'кнопка': 'card__category_button',
+  'дополнительное': 'card__category_additional',
+  'другое': 'card__category_other',
+};
+
 export const settings = {
 
 };
