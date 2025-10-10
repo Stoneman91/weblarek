6d86409a805ad99367ee941f8263901be3c529a1
Nick Boickov <nick.boickov@yandex.ru>
2025-10-10 08:29:32 +0300

    PR9_card+cardCatalog+gallery

---
+++ src/components/view/card.ts
@@ -0,0 +1,26 @@
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+
+export interface ICardActions {
+    onClick?: (event: MouseEvent) => void;
+}
+
+export abstract class Card<T> extends Component<T> {
+    protected _title: HTMLElement;
+    protected _price: HTMLElement;
+
+    constructor(protected container: HTMLElement) {
+        super(container);
+        this._title = ensureElement<HTMLElement>('.card__title', this.container);
+        this._price = ensureElement<HTMLElement>('.card__price', this.container);
+    }
+
+    set title(value: string) {
+        this._title.textContent = value;
+    }
+
+    set price(value: string | number) {
+        const priceText = typeof value === 'number' ? `${value} синапсов` : value;
+        this._price.textContent = priceText;
+    }
+}
