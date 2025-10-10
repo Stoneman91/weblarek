cfed6ba3ccc2cb4d3a4f6bd878f444ea5c77c45b
Nick Boickov <nick.boickov@yandex.ru>
2025-10-10 09:25:25 +0300

    PR9_cardBasket

---
+++ src/components/view/cardBasket.ts
@@ -0,0 +1,22 @@
+import { ensureElement } from "../../utils/utils";
+import { Card, ICardActions } from "./card";
+
+export class CardBasket extends Card<ICardActions> {
+    protected _index: HTMLElement;
+    protected _deleteButton: HTMLButtonElement;
+
+    constructor(container: HTMLElement, actions?: ICardActions) {
+        super(container);
+        
+        this._index = ensureElement<HTMLElement>('.basket__item-index', this.container);
+        this._deleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', this.container);
+        
+        if (actions?.onClick) {
+            this._deleteButton.addEventListener('click', actions.onClick);
+        }
+    }
+
+    set index(value: number) {
+        this._index.textContent = String(value);
+    }
+}
